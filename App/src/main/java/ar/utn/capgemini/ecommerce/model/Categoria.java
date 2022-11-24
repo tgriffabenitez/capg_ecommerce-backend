c902923e08a41a7362787159ed050a69507967ba
tgriffabenitez <tgriffabenitez@frba.utn.edu.ar>
2022-11-23 23:16:44 -0300

    clase categoria base

--- App/src/main/java/ar/utn/capgemini/ecommerce/model/Categoria.java
+++ App/src/main/java/ar/utn/capgemini/ecommerce/model/Categoria.java
@@ -1,5 +1,30 @@
 package ar.utn.capgemini.ecommerce.model;
 
-public class Categoria {
+import ar.utn.capgemini.ecommerce.model.persist.EntidadPersistente;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "categoria")
+public class Categoria extends EntidadPersistente {
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @Transient
+    private List<ProductoBase> productosBase;
+
+    public Categoria() {
+        this.descripcion = descripcion;
+        productosBase = new ArrayList<>();
+    }
 }
