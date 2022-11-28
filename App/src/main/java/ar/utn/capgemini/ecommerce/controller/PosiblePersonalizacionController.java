b68a7d437a8afe8ffe63ddf60d972f5e7a3244f2
tgriffabenitez <tgriffabenitez@frba.utn.edu.ar>
2022-11-28 19:59:05 -0300

    El programa funciona bien. Pero la posiblePersonalizacion no tiene id unico. Se sigue repitiendo

--- App/src/main/java/ar/utn/capgemini/ecommerce/controller/PosiblePersonalizacionController.java
+++ App/src/main/java/ar/utn/capgemini/ecommerce/controller/PosiblePersonalizacionController.java
@@ -1,9 +1,11 @@
 package ar.utn.capgemini.ecommerce.controller;
 
+import ar.utn.capgemini.ecommerce.model.entities.AreaPersonalizacion;
 import ar.utn.capgemini.ecommerce.model.entities.PosiblePersonalizacion;
-import ar.utn.capgemini.ecommerce.model.entities.ProductoBase;
+import ar.utn.capgemini.ecommerce.model.entities.TipoPersonalizacion;
+import ar.utn.capgemini.ecommerce.repository.AreaPersonalizacionRepository;
 import ar.utn.capgemini.ecommerce.repository.PosiblePersonalizacionRepository;
-import ar.utn.capgemini.ecommerce.repository.ProductoBaseRepository;
+import ar.utn.capgemini.ecommerce.repository.TipoPersonalizacionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,13 +18,28 @@ public class PosiblePersonalizacionController {
     @Autowired
     public PosiblePersonalizacionRepository posiblePersonalizacionRepository;
 
+    @Autowired
+    public TipoPersonalizacionRepository tipoPersonalizacionRepository;
+
+    @Autowired
+    public AreaPersonalizacionRepository areaPersonalizacionRepository;
+
     @GetMapping(path = {"", "/"})
     public List<PosiblePersonalizacion> obtenerPosiblePersonalizacion(){
         return posiblePersonalizacionRepository.findAll();
     }
 
+    // FIXME: Buscar la forma en que en caso de existir el tipo y area no genere un nuevo id
     @PostMapping(path = {"", "/"})
     public PosiblePersonalizacion agregarProductoBase(@RequestBody PosiblePersonalizacion posiblePersonalizacion){
+        TipoPersonalizacion tipoPersonalizacion = posiblePersonalizacion.getTipoPersonalizacion();
+        AreaPersonalizacion areaPersonalizacion = posiblePersonalizacion.getAreaPersonalizacion();
+        boolean existeTipo = tipoPersonalizacionRepository.existsByDescripcion(tipoPersonalizacion.getDescripcion());
+        boolean existeArea = tipoPersonalizacionRepository.existsByDescripcion(areaPersonalizacion.getDescripcion());
+        if(existeTipo && existeArea){
+            return  posiblePersonalizacionRepository.findByTipoPersonalizacionAndAreaPersonalizacion(tipoPersonalizacion, areaPersonalizacion);
+        }
+
         return posiblePersonalizacionRepository.save(posiblePersonalizacion);
     }
 
--- App/src/main/java/ar/utn/capgemini/ecommerce/repository/PosiblePersonalizacionRepository.java
+++ App/src/main/java/ar/utn/capgemini/ecommerce/repository/PosiblePersonalizacionRepository.java
@@ -1,9 +1,15 @@
 package ar.utn.capgemini.ecommerce.repository;
 
+import ar.utn.capgemini.ecommerce.model.entities.AreaPersonalizacion;
 import ar.utn.capgemini.ecommerce.model.entities.PosiblePersonalizacion;
+import ar.utn.capgemini.ecommerce.model.entities.TipoPersonalizacion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
 @RepositoryRestResource(path = "posiblesPersonalizaciones")
 public interface PosiblePersonalizacionRepository extends JpaRepository<PosiblePersonalizacion, Integer> {
+
+    PosiblePersonalizacion findByTipoPersonalizacionAndAreaPersonalizacion(TipoPersonalizacion tipoPersonalizacion, AreaPersonalizacion areaPersonalizacion);
+
 }
