f6af9e201a3edd641de5d5a85118ab107a60d714
tgriffabenitez <tgriffabenitez@frba.utn.edu.ar>
2022-12-05 09:48:05 -0300

    Cambie el nombre de la tabla de compra a carrito. Cambie el tipo de dato para los precios de double a BigDecimal. Elimine la hora de compra ya que el LocalDateTime me da toda la informacion.

--- App/src/main/java/ar/utn/capgemini/ecommerce/model/entities/Carrito.java
+++ App/src/main/java/ar/utn/capgemini/ecommerce/model/entities/Carrito.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.*;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +16,7 @@ import java.util.List;
 @Getter
 @Setter
 @Entity
-@Table(name = "compra")
+@Table(name = "carrito")
 public class Carrito extends EntidadPersistente {
 
     @NotNull
@@ -23,10 +24,6 @@ public class Carrito extends EntidadPersistente {
     @Column(name = "fechaDeCompra", columnDefinition = "DATETIME")
     private LocalDateTime fechaDeCompra;
 
-    @NotNull
-    @Past
-    @Column(name = "horaDeCompra", columnDefinition = "DATETIME")
-    private LocalDateTime horaDeCompra;
 
     @NotNull
     @Enumerated(EnumType.STRING)
@@ -44,15 +41,15 @@ public class Carrito extends EntidadPersistente {
     @NotNull
     @Positive
     @Column(name = "precioTotal")
-    private double precioTotal;
+    private BigDecimal precioTotal;
 
     @OneToMany
     @JoinColumn(name = "publicacionCarrito", referencedColumnName = "id")
     private List<PublicacionCarrito> publicacionesCarrito;
 
-    public Carrito(LocalDateTime fechaDeCompra, LocalDateTime horaDeCompra, PAGO metodoDePago, ESTADO estadoDeCompra, LocalDateTime fechaCambioEstado, double precioTotal) {
+    public Carrito(LocalDateTime fechaDeCompra, PAGO metodoDePago, ESTADO estadoDeCompra, LocalDateTime fechaCambioEstado, BigDecimal precioTotal) {
+        this.publicacionesCarrito = new ArrayList<>();
         this.fechaDeCompra = fechaDeCompra;
-        this.horaDeCompra = horaDeCompra;
         this.metodoDePago = metodoDePago;
         this.estadoDeCompra = estadoDeCompra;
         this.fechaCambioEstado = fechaCambioEstado;
