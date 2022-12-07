2a3b5b45e8d5413e5124069cd4dac1d350205f91
tgriffabenitez <tgriffabenitez@frba.utn.edu.ar>
2022-12-07 13:00:29 -0300

    Cree el controller para las publicaciones. Verificar si funciona

--- App/src/main/java/ar/utn/capgemini/ecommerce/controller/PublicacionController.java
+++ App/src/main/java/ar/utn/capgemini/ecommerce/controller/PublicacionController.java
@@ -1,4 +1,65 @@
 package ar.utn.capgemini.ecommerce.controller;
 
+import ar.utn.capgemini.ecommerce.model.dto.PublicacionDTO;
+import ar.utn.capgemini.ecommerce.model.entities.ProductoPersonalizado;
+import ar.utn.capgemini.ecommerce.model.entities.Publicacion;
+import ar.utn.capgemini.ecommerce.repository.ProductoPersonalizadoRepository;
+import ar.utn.capgemini.ecommerce.repository.PublicacionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/publicacion")
 public class PublicacionController {
-}
+    @Autowired
+    private PublicacionRepository publicacionRepository;
+    @Autowired
+    private ProductoPersonalizadoRepository productoPersonalizadoRepository;
+
+    @GetMapping(path = {"", "/"})
+    public Page<Publicacion> obtenerPublicaciones(Pageable pagina) {
+        return publicacionRepository.findAll(pagina);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> darPublicacionDeBaja(@PathVariable Integer id) {
+        if (!publicacionRepository.findById(id).isPresent())
+            return new ResponseEntity<>("No existe la publicacion", HttpStatus.NOT_FOUND);
+
+        Publicacion publicacion = publicacionRepository.findById(id).get();
+        publicacion.setEstaActivo(false);
+        publicacion.setFechaDeBaja(LocalDateTime.now());
+        publicacion.setFechaCambioEstado(LocalDateTime.now());
+        publicacionRepository.save(publicacion);
+
+        return new ResponseEntity<>("Publicacion dada de baja", HttpStatus.OK);
+    }
+
+    @PatchMapping(path = "/{id}")
+    public ResponseEntity<?> actualizarPublicacion(@PathVariable Integer id, @RequestBody @Valid PublicacionDTO publicacion) {
+        if (!publicacionRepository.findById(id).isPresent())
+            return new ResponseEntity<>("No existe la publicacion", HttpStatus.NOT_FOUND);
+
+        if (!productoPersonalizadoRepository.findById(publicacion.getProductoPersonalizadoId()).isPresent())
+            return new ResponseEntity<>("No existe el producto personalizado", HttpStatus.NOT_FOUND);
+
+        Publicacion publicacionActualizada = publicacionRepository.findById(id).get();
+        ProductoPersonalizado productoPersonalizado = productoPersonalizadoRepository.findById(publicacion.getProductoPersonalizadoId()).get();
+
+        publicacionActualizada.setTitulo(publicacion.getTitulo());
+        publicacionActualizada.setDescripcion(publicacion.getDescripcion());
+        publicacionActualizada.setPrecio(publicacion.getPrecio());
+        publicacionActualizada.setProductoPersonalizado(productoPersonalizado);
+        publicacionActualizada.setFechaCambioEstado(LocalDateTime.now());
+        publicacionRepository.save(publicacionActualizada);
+
+        return new ResponseEntity<>("Publicacion actualizada", HttpStatus.OK);
+    }
+} // fin PublicacionController
