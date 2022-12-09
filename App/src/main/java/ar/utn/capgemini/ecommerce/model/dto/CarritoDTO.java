e63543448ae388659f2c6de6204c8ce2241f5ec7
tgriffabenitez <tgriffabenitez@frba.utn.edu.ar>
2022-12-09 12:44:58 -0300

    CarritoDTO completo. FuncionaTodo ok

--- App/src/main/java/ar/utn/capgemini/ecommerce/model/dto/CarritoDTO.java
+++ App/src/main/java/ar/utn/capgemini/ecommerce/model/dto/CarritoDTO.java
@@ -1,38 +1,24 @@
 package ar.utn.capgemini.ecommerce.model.dto;
 
-import ar.utn.capgemini.ecommerce.model.entities.Carrito;
-import ar.utn.capgemini.ecommerce.model.utils.ESTADO;
-import ar.utn.capgemini.ecommerce.model.utils.PAGO;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
-import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @Setter
 public class CarritoDTO {
+    @Positive
     @NotNull
-    @PastOrPresent
-    private LocalDateTime fechaDeCompra;
+    private Integer clienteId;
     @NotNull
-    @Enumerated(EnumType.STRING)
-    private PAGO metodoDePago;
-    @NotNull
-    private ESTADO estadoDeCompra;
-    @NotNull
-    @Past
-    private LocalDateTime fechaCambioEstado;
+    private Integer metodoDePagoId;
     @NotNull
     @Positive
     private BigDecimal precioTotal;
     @NotNull
-    private List<Carrito> carritos;
+    private List<PublicacionCarritoDTO> publicaciones;
 }
