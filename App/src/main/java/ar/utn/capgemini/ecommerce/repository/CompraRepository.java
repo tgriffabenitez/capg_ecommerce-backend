765c3b76d249fdd086a9dc84e82bf4f7a999962e
tgriffabenitez <tgriffabenitez@frba.utn.edu.ar>
2022-12-09 15:29:55 -0300

    Cambie de nombre los archivos Venta por Compra

--- App/src/main/java/ar/utn/capgemini/ecommerce/repository/CompraRepository.java
+++ App/src/main/java/ar/utn/capgemini/ecommerce/repository/CompraRepository.java
@@ -1,10 +1,10 @@
 package ar.utn.capgemini.ecommerce.repository;
 
-import ar.utn.capgemini.ecommerce.model.entities.Venta;
+import ar.utn.capgemini.ecommerce.model.entities.Compra;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VentaRepository extends JpaRepository<Venta, Integer> {
+public interface CompraRepository extends JpaRepository<Compra, Integer> {
 
 }
