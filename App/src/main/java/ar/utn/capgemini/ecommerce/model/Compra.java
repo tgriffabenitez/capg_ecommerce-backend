42ced5e83cc08b1efb6393bff08d34aad7e3ef76
tgriffabenitez <tgriffabenitez@frba.utn.edu.ar>
2022-12-10 15:37:54 -0300

    Cree la entidad compra con las relaciones

--- App/src/main/java/ar/utn/capgemini/ecommerce/model/Compra.java
+++ App/src/main/java/ar/utn/capgemini/ecommerce/model/Compra.java
@@ -1,11 +1,15 @@
 package ar.utn.capgemini.ecommerce.model;
 
 import ar.utn.capgemini.ecommerce.utils.EntidadPersistente;
+import ar.utn.capgemini.ecommerce.utils.PAGO;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Positive;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -13,5 +17,22 @@ import javax.persistence.Table;
 @Table(name = "compra")
 public class Compra extends EntidadPersistente {
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "formaDePago")
+    private PAGO metodoDePago;
 
+    @Positive
+    @Column(name = "precioTotal")
+    private BigDecimal precioTotal;
+
+    @PastOrPresent
+    @Column(name = "fechaDeCompra", columnDefinition = "DATETIME")
+    private LocalDateTime fechaDeCompra;
+
+    @OneToOne
+    @JoinColumn(name = "carrito_id", referencedColumnName = "id")
+    private Carrito carrito;
+
+    public Compra() {
+    }
 }
