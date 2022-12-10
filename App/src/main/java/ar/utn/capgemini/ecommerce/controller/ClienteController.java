f8e9205e00ca3f7eb0771f05ee66e9a7e50a78b4
tgriffabenitez <tgriffabenitez@frba.utn.edu.ar>
2022-12-10 15:10:55 -0300

    Elimine la logica de servicios. Hago todo en el controller. Funciona bien

--- App/src/main/java/ar/utn/capgemini/ecommerce/controller/ClienteController.java
+++ App/src/main/java/ar/utn/capgemini/ecommerce/controller/ClienteController.java
@@ -1,7 +1,8 @@
 package ar.utn.capgemini.ecommerce.controller;
 
 import ar.utn.capgemini.ecommerce.dto.ClienteDTO;
-import ar.utn.capgemini.ecommerce.service.ClienteService;
+import ar.utn.capgemini.ecommerce.model.Cliente;
+import ar.utn.capgemini.ecommerce.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,11 +16,11 @@ import javax.validation.Valid;
 public class ClienteController {
 
     @Autowired
-    private ClienteService clienteService;
+    private ClienteRepository clienteRepository;
 
     @GetMapping(path = "")
     public ResponseEntity<?> obtenerClientes() {
-        return clienteService.findAll();
+        return new ResponseEntity<>(clienteRepository.findAll(), HttpStatus.OK);
     }
 
     @PostMapping(path = {"", "/"})
@@ -28,10 +29,20 @@ public class ClienteController {
         if (bindingResult.hasErrors())
             return new ResponseEntity<>(bindingResult.getAllErrors(), HttpStatus.BAD_REQUEST);
 
-        if (clienteService.findByEmail(clienteDTO.getEmail()).isPresent())
+        if (clienteRepository.findByEmail(clienteDTO.getEmail()).isPresent())
             return new ResponseEntity<>("El email ya esta registrado", HttpStatus.BAD_REQUEST);
 
-        clienteService.agregarCliente(clienteDTO, clienteDTO.getEmail());
+        Cliente cliente = new Cliente();
+        cliente.setNombre(clienteDTO.getNombre());
+        cliente.setApellido(clienteDTO.getApellido());
+        cliente.setEmail(clienteDTO.getEmail());
+        cliente.setTelefono(clienteDTO.getTelefono());
+        cliente.setContrasenia(clienteDTO.getContrasenia());
+        cliente.setDireccionCalle(clienteDTO.getDireccionCalle());
+        cliente.setDireccionNumero(clienteDTO.getDireccionNumero());
+        cliente.setDireccionPiso(clienteDTO.getDireccionPiso());
+        cliente.setDireccionDepto(clienteDTO.getDireccionDepto());
+        clienteRepository.save(cliente);
         return new ResponseEntity<>("Cliente registrado con exito", HttpStatus.CREATED);
     }
 } // fin ClienteController
