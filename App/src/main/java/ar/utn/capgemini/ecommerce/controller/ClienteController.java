27887658adefc50436e5c0488b76c0de8061cde4
tgriffabenitez <tgriffabenitez@frba.utn.edu.ar>
2022-12-21 09:53:08 -0300

    cree un metodo post para validar el login del usuario

--- App/src/main/java/ar/utn/capgemini/ecommerce/controller/ClienteController.java
+++ App/src/main/java/ar/utn/capgemini/ecommerce/controller/ClienteController.java
@@ -1,6 +1,7 @@
 package ar.utn.capgemini.ecommerce.controller;
 
 import ar.utn.capgemini.ecommerce.dto.ClienteDTO;
+import ar.utn.capgemini.ecommerce.dto.LoginDTO;
 import ar.utn.capgemini.ecommerce.model.Cliente;
 import ar.utn.capgemini.ecommerce.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,19 +19,17 @@ public class ClienteController {
     @Autowired
     private ClienteRepository clienteRepository;
 
-    @GetMapping(path = "")
-    public ResponseEntity<?> obtenerClientes(@RequestParam(name = "email", required = false) String email,
-                                             @RequestParam(name = "password", required = false) String password) {
 
-        if (email != null && password != null) {
-            Cliente cliente = clienteRepository.findByEmailAndContrasenia(email, password);
-            if (cliente == null) {
-                return new ResponseEntity<>("Usuario o contraseña incorrectos", HttpStatus.BAD_REQUEST);
-            }
-            return new ResponseEntity<>(cliente, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(clienteRepository.findAll(), HttpStatus.BAD_REQUEST);
+    @PostMapping(path = "/login")
+    public ResponseEntity<?> iniciarSesion(@Valid @RequestBody LoginDTO loginDTO, BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return new ResponseEntity<>(bindingResult.getAllErrors(), HttpStatus.BAD_REQUEST);
+
+        Cliente cliente = clienteRepository.findByEmailAndContrasenia(loginDTO.getEmail(), loginDTO.getContrasenia());
+        if (cliente == null) {
+            return new ResponseEntity<>("Usuario o contraseña incorrectos", HttpStatus.BAD_REQUEST);
         }
+        return new ResponseEntity<>(cliente, HttpStatus.OK);
     }
 
     @PostMapping(path = {"", "/"})
